acbc9df5cc26f86d58ab584df4856ab6792cfe1b
Tim Chipperfield <tim@trueview.me>
2019-09-25 21:18:10 -0700

    added a few more conditions to original feature spec

--- spec/features/products/index_spec.rb
+++ spec/features/products/index_spec.rb
@@ -4,13 +4,17 @@ RSpec.describe 'vistiting the list of products', type: :feature do
   describe "GET index" do
     context "when there are products" do
       let(:title) { "Ferrari Enzo" }
-      let(:product) { create(:product, title: title) }
+      let(:rank) { 4.3 }
+      let(:category) { "cars" }
+      let(:product) { create(:product, title: title, rank: rank, category: category) }
       before { product }
 
       it "shows the product" do
         visit '/products'
 
         expect(page).to have_content(title)
+        expect(page).to have_content(rank)
+        expect(page).to have_content(category)
       end
     end
   end
