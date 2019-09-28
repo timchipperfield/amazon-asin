855cdac412af4e4137449f682e08fe46e969393a
Tim Chipperfield <tim@trueview.me>
2019-09-28 11:39:58 -0700

    added parsing of scraped content

---
+++ lib/content_parsable.rb
@@ -0,0 +1,26 @@
+module ContentParsable
+  def parse_title(raw_title)
+    regex = %r{:\s(?<title>.+)\s:}
+    matches = raw_title.match(regex)
+    return "" unless matches
+
+    matches[:title]
+  end
+
+  def parse_category(raw_category)
+    regex = %r{(?<category>\w+(.\w+)*)}
+    matches = raw_category.match(regex)
+    return "" unless matches
+
+    matches[:category]
+  end
+
+
+  def parse_rank(raw_rank)
+    regex = %r{(?<rank>#.+?)(?=\s\()}
+    matches = raw_rank.match(regex)
+    return "" unless matches
+
+    matches[:rank]
+  end
+end
--- spec/services/scraper_service_spec.rb
+++ spec/services/scraper_service_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe ScraperService, type: :model do
     context "when a real asin is passed to the service", vcr: {cassette_name: "success_request"} do
       let(:asin) { "B002QYW8LW" }
 
-      it "returns a product hash" do
-        expect(scraper.scrape[:raw_category]).to include("Baby Products")
-        expect(scraper.scrape[:raw_rank]).to include("#28 in Baby")
-        expect(scraper.scrape[:raw_title]).to include("Baby Banana Infant Training Toothbrush and Teether")
+      it "returns a product hash", :aggregate_failures do
+        scraped_content = scraper.scrape
+        expect(scraped_content[:category]).to eq("Baby Products")
+        expect(scraped_content[:rank]).to eq("#28 in Baby")
+        expect(scraped_content[:title]).to eq("Baby Banana Infant Training Toothbrush and Teether")
       end
     end
 
